e5bb35c924f11e3bfaa7c2de082a5c44794dff87
dhrurast <dhrurast@gmail.com>
2025-12-09 19:19:28 +0530

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>index</Text>
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 20,
+        color: '#333',
+    },
+})
